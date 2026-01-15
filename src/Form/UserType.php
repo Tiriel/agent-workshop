23754c45049febd979885f3555a379b321347c6e
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2026-01-15 16:01:19 +0100

    Add tests

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,14 +14,22 @@ class UserType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('roles')
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'ROLE_USER' => 'ROLE_USER',
+                    'ROLE_CONTRIBUTOR' => 'ROLE_CONTRIBUTOR',
+                    'ROLE_ADMIN' => 'ROLE_ADMIN'
+                ],
+                'data' => ['ROLE_USER' => 'ROLE_USER'],
+                'expanded' => false,
+                'multiple' => true,
+            ])
             ->add('plainPassword', \Symfony\Component\Form\Extension\Core\Type\PasswordType::class, [
                 'mapped' => false,
                 'required' => $options['method'] === 'POST', // optional on edit
             ])
             ->add('firstname')
-            ->add('lastname')
-        ;
+            ->add('lastname');
     }
 
     public function configureOptions(OptionsResolver $resolver): void
